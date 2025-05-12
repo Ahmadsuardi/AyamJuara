c7eb202262f0aaf538065535bb1c7c7c3c0a798a
ahmadsuardi <ahmadsuardi044@gmail.com>
2025-05-12 18:12:32 +0800

    style: integrasi template

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,123 @@
+<!-- <!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="font-sans antialiased">
+        <div class="min-h-screen bg-gray-100">
+            @include('layouts.navigation')
+            @isset($header)
+                <header class="bg-white shadow">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        {{ $header }}
+                    </div>
+                </header>
+            @endisset
+
+            <main>
+                {{ $slot }}
+            </main>
+        </div>
+    </body>
+</html> -->
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Live Preview | Gradient Able Dashboard Template</title>
+    <!-- [Meta] -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta
+      name="description"
+      content="Gradient Able is trending dashboard template made using Bootstrap 5 design framework. Gradient Able is available in Bootstrap, React, CodeIgniter, Angular,  and .net Technologies."
+    />
+    <meta
+      name="keywords"
+      content="Bootstrap admin template, Dashboard UI Kit, Dashboard Template, Backend Panel, react dashboard, angular dashboard"
+    />
+    <meta name="author" content="codedthemes" />
+
+    <!-- [Favicon] icon -->
+    <link rel="icon" href="assets/images/favicon.svg" type="image/x-icon" />
+    <!-- [Google Font : Poppins] icon -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet" />
+    <!-- [Tabler Icons] https://tablericons.com -->
+    <link rel="stylesheet" href="assets/fonts/tabler-icons.min.css" />
+    <!-- [Feather Icons] https://feathericons.com -->
+    <link rel="stylesheet" href="assets/fonts/feather.css" />
+    <!-- [Font Awesome Icons] https://fontawesome.com/icons -->
+    <link rel="stylesheet" href="assets/fonts/fontawesome.css" />
+    <!-- [Material Icons] https://fonts.google.com/icons -->
+    <link rel="stylesheet" href="assets/fonts/material.css" />
+    <!-- [Template CSS Files] -->
+    <link rel="stylesheet" href="assets/css/style.css" id="main-style-link" />
+    <link rel="stylesheet" href="assets/css/style-preset.css" />
+
+    <link rel="stylesheet" href="assets/css/landing.css" />
+  </head>
+
+  <body
+    data-pc-header="header-1"
+    data-pc-preset="preset-1"
+    data-pc-sidebar-theme="light"
+    data-pc-sidebar-caption="true"
+    data-pc-direction="ltr"
+    data-pc-theme="light"
+    class="landing-page"
+  >
+    <!-- [ Main Content ] start -->
+    <!-- [ Pre-loader ] start -->
+    <div class="loader-bg">
+      <div class="loader-track">
+        <div class="loader-fill"></div>
+      </div>
+    </div>
+    <!-- [ Pre-loader ] End -->
+
+    @include('layouts.header')
+
+    <!-- [ Header ] End -->
+    <!-- [ Main Content ] end -->
+    <!-- Required Js -->
+    <script src="assets/js/plugins/popper.min.js"></script>
+    <script src="assets/js/plugins/simplebar.min.js"></script>
+    <script src="assets/js/plugins/bootstrap.min.js"></script>
+    <script src="assets/js/fonts/custom-font.js"></script>
+    <script src="assets/js/script.js"></script>
+    <script src="assets/js/theme.js"></script>
+    <script src="assets/js/plugins/feather.min.js"></script>
+    <script>
+      layout_change('light');
+    </script>
+    <script>
+      layout_sidebar_change('light');
+    </script>
+    <script>
+      change_box_container('false');
+    </script>
+    <script>
+      layout_caption_change('true');
+    </script>
+    <script>
+      layout_rtl_change('false');
+    </script>
+    <script>
+      preset_change('preset-1');
+    </script>
+    <script>
+      header_change('header-1');
+    </script>
+  </body>
+</html>
+
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,64 @@
+<!-- [ Header ] start -->
+    <header id="home" class="bg-white">
+      <!-- [ Nav ] start -->
+      <!-- [ Nav ] start -->
+      <nav class="navbar navbar-expand-lg navbar-dark default">
+        <div class="container">
+          <a class="navbar-brand" href="index.html">
+            <img src="assets/images/logo-white.svg" alt="logo" class="logo-lg" />
+          </a>
+          <button
+            class="navbar-toggler rounded"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <ul class="navbar-nav ms-auto me-auto mb-2 mb-lg-0 align-items-start">
+              <li class="nav-item px-1">
+                <a class="nav-link" href="https://codedthemes.gitbook.io/gradient-able-bootstrap/" target="_blank">Documentation</a>
+              </li>
+              <li class="nav-item px-1">
+                <a class="nav-link" href="dashboard/index.html">Live Preview</a>
+              </li>
+            </ul>
+            <ul class="navbar-nav mb-2 mb-lg-0 align-items-start">
+              <li class="nav-item">
+                <a href="https://codedthemes.com/item/gradient-able-admin-template/" target="_blank" class="btn btn-success"
+                  >Get Gradient Able <i class="ph ph-arrow-square-out"></i
+                ></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <!-- [ Nav ] start -->
+
+      <!-- [ Nav ] start -->
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-5 header-content">
+            <h6 class="mb-0 wow animate__fadeInUp" data-wow-delay="0.2s">Most Complete, Fast and Robust Flat User Interface</h6>
+            <h1 class="my-3 wow animate__fadeInUp" data-wow-delay="0.4s">Friends with Gradient Able</h1>
+            <p class="text-muted f-16 my-3 wow animate__fadeInUp" data-wow-delay="0.6s"
+              >Gradient Able is the first released flat UI design admin template which made using Bootstrap 5 design framework.Most
+              productive feature rich dashboard template ever made in Bootstrap 5.
+            </p>
+            <div class="wow animate__fadeInUp" data-wow-delay="0.8s">
+              <a href="dashboard/index.html" class="btn btn-dark me-2">Preview</a>
+              <a href="https://codedthemes.com/item/gradient-able-admin-template/" target="_blank" class="btn btn-primary">Buy Now</a>
+            </div>
+          </div>
+          <div class="col-md-7 wow animate__fadeInRight" data-wow-delay="0.4s">
+            <div class="card img-header">
+              <img src="assets/images/landing/img-header-moke.png" alt="img" class="img-fluid" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
